9ab41664c2bf27b3599be0e0e878b4ec271e6656
liqMix <liqmix@protonmail.com>
2023-07-02 18:18:02 -0600

    adjust max_token calc

--- src/gpt.py
+++ src/gpt.py
@@ -4,16 +4,18 @@ from .config import Config
 
 class ChatGPT:
     def __init__(self) -> None:
-      openai.api_key = Config.gpt_api_key
+        openai.api_key = Config.gpt_api_key
 
     def get_response(self, system_prompt: str, prompt: str):
-        max_tokens = Config.max_tokens - len(system_prompt) - len(prompt)
+        s_prompt = {"role": "system", "content": system_prompt}
+        u_prompt = {"role": "user", "content": prompt}
+        max_tokens = Config.max_tokens - len(str(s_prompt)) - len(str(u_prompt))
         completion = openai.ChatCompletion.create(
             model=Config.gpt_model,
             max_tokens=max_tokens,
             messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": prompt},
+                s_prompt,
+                u_prompt,
             ],
         )
         return completion.choices[0].message["content"]
